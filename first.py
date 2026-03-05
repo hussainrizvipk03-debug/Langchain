2e431c0fc1d0139b8cb26845df2bf1b54fb3cd03
hussainrizvipk03 <hussainrizvipk03@gmail.com>
2026-03-06 01:51:45 +0500

    these are the changes made

--- first.py
+++ first.py
@@ -4,14 +4,15 @@ from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage,AIMessage
-
+import tiktoken
+encoder = tiktoken.encoding_for_model("gpt-4o-mini")
 chat_history = []
 q_1 = "Hi My name is Hussain Adnan, How are you?"
 r_1 = "Hey how are you doing I am well , Pleasure to meet you Hussain."
 chat_history.append(HumanMessage(content=q_1))
 chat_history.append(AIMessage(content=r_1))
 load_dotenv()
-llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.7,max_tokens=110)
+llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.7,max_tokens=20)
 prompt = ChatPromptTemplate.from_messages([
     ('system', "You are a AI Engineer help me understand topics"),
     ('placeholder', "{chat_history}"),
@@ -22,6 +23,15 @@ parser = StrOutputParser()
 
 chain = prompt | llm | parser
 response = chain.invoke({"chat_history": chat_history , "input": 'what is Machine Learning'})
+
+formated_messages = prompt.format_messages(chat_history=chat_history, input="what is Machine Learning")
+input_tokens = sum(len(encoder.encode(msg.content)) for msg in formated_messages)
+output_tokens = len(encoder.encode(response))
+
+print(f"no of input tokens:  {input_tokens}")
+print(f"no of output tokens: {output_tokens}")
+
+
 
 print(prompt)
 print("-"*20)
